b593bbfc9d4e9d6493dd8aa6f3c7d05dc6e450c4
Ravitheja <gollaravitheja00@gmail.com>
2026-06-22 13:02:06 +0530

    base route app

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -15,7 +15,9 @@ app.use(express.json())
 
 app.use('/api/task', taskRouter)
 
-
+app.get('/', (req, res) => {
+    res.send('Task Manager API is running')
+})
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
 
